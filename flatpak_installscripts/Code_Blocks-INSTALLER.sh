5ace8e41d280bc99857f7657d73d46102b4b4589
ConzZah <69615452+ConzZah@users.noreply.github.com>
2024-06-21 11:48:02 +0200

    fixed an error

--- flatpak_installscripts/Code_Blocks-INSTALLER.sh
+++ flatpak_installscripts/Code_Blocks-INSTALLER.sh
@@ -17,20 +17,20 @@ if [[ "$_DE" == "GNOME" ]]; then $_doso $add $_portal-gnome; fi
 if [[ "$_DE" == "LXQT" ]]; then $_doso $add $_portal-lxqt; fi
 if [[ "$_DE" == "KDE" ]]; then $_doso $add $_portal-kde; fi
 $_doso $add $y flatpak && $_doso flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
-$_doso flatpak install -y flathub org.audacityteam.Audacity
+$_doso flatpak install -y flathub org.codeblocks.codeblocks
 cd /home/$USER/Desktop
 echo ""; echo "CREATING DESKTOP SHORTCUT..."
-wget -q -O Audacity_icon.png https://dl.flathub.org/media/org/audacityteam/Audacity/b5bc69f79fc7f8f95acf532388b6257a/icons/128x128/org.audacityteam.Audacity.png
-$_doso mv Audacity_icon.png /etc/
-_sc="Audacity.desktop"
+wget -q -O Code::Blocks-icon.png https://dl.flathub.org/repo/appstream/x86_64/icons/128x128/org.codeblocks.codeblocks.png
+$_doso mv Code::Blocks-icon.png /etc/
+_sc="Code::Blocks.desktop"
 cd /home/$USER/Desktop
 echo "[Desktop Entry]">$_sc
 echo "Version=1.0">>$_sc
 echo "Type=Application">>$_sc
-echo "Name=Audacity">>$_sc
-echo "Comment=Audacity">>$_sc
-echo "Exec=flatpak run org.audacityteam.Audacity">>$_sc
-echo "Icon=/etc/Audacity_icon.png">>$_sc
+echo "Name=Code::Blocks">>$_sc
+echo "Comment=Code::Blocks">>$_sc
+echo "Exec=flatpak run org.codeblocks.codeblocks">>$_sc
+echo "Icon=/etc/Code::Blocks-icon.png">>$_sc
 echo "Path=">>$_sc
 echo "Terminal=false">>$_sc
 echo "StartupNotify=false">>$_sc
